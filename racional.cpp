f4ff832d002d4c7721c4ba49ae6f0dc919e76e6e
Cesconcins <cesco.oncins@gmail.com>
2022-11-22 16:23:47 +0100

    Cesco

--- racional.cpp
+++ racional.cpp
@@ -78,7 +78,7 @@ racional racional::residu() const throw()
 /* Sobrecàrrega d'operadors aritmètics. Retorna un racional en la seva
    versió simplificada amb el resultat de l'operació. Es produeix un
    error al dividir dos racionals si el segon és 0.*/
-racional racional::operator+(const racional &r) throw(error)
+racional racional::operator+(const racional &r) const throw(error)
 {
     int min, den = _d, r_d = r.denom();
     min = mcm(r_d, den);
