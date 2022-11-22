ed161e350cc5853896825529b775e7a58deb1ecf
Cesconcins <cesco.oncins@gmail.com>
2022-11-22 16:07:31 +0100

    Cesco

--- racional.cpp
+++ racional.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 // Constructora. Construeix un racional en la seva versió simplificada.
 // Es produeix un error si el denominador és 0.
-racional::racional(int n, int d) throw(error){
+racional::racional(int n, int d) throw(error)
+{
     _d = d;
     _n = n;
 }
@@ -13,20 +14,22 @@ racional::racional(int n, int d) throw(error){
 // Constructora per còpia, assignació i destructora.
 racional::racional(const racional &r) throw(error)
 {
-    if(r._d == 0){
+    if (r._d == 0)
+    {
         throw(21);
     }
     _d = r._d;
     _n = r._d;
 }
-racional & racional::operator=(const racional &r) throw(error)
+racional &racional::operator=(const racional &r) throw(error)
 {
-    if(r._d == 0){
+    if (r._d == 0)
+    {
         throw(21);
     }
     _d = r._d;
     _n = r._d;
-    return *this; 
+    return *this;
 }
 racional::~racional() throw()
 {
@@ -45,7 +48,6 @@ int racional::denom() const throw()
 int racional::part_entera() const throw()
 {
     return _n / _d;
-    
 }
 racional racional::residu() const throw()
 {
@@ -53,13 +55,15 @@ racional racional::residu() const throw()
     racional r;
     aux = _n % _d;
     int c = 0;
-    while ((int)x != x) {  
-        x *= 10; 
-        c++; 
+    while ((int)x != x)
+    {
+        x *= 10;
+        c++;
     }
     int deno = 1;
-    for(int i = 0; i < c-1; i++){
-        deno = deno*10;
+    for (int i = 0; i < c - 1; i++)
+    {
+        deno = deno * 10;
     }
     r._n = (int)x;
     r._d = deno;
@@ -72,18 +76,18 @@ racional racional::residu() const throw()
    error al dividir dos racionals si el segon és 0.*/
 racional racional::operator+(const racional &r) const throw(error)
 {
-    int min; 
+    int min;
     min = mcm(r._d, _d);
-    _n = (_n*min) + (r._n);
+    _n = (_n * min) + (r._n);
     _d = min;
     simplificar();
     return *this;
 }
 racional racional::operator-(const racional &r) const throw(error)
 {
-    int min; 
+    int min;
     min = mcm(r._d, _d);
-    _n = (_n*min) - (r._n);
+    _n = (_n * min) - (r._n);
     _d = min;
     simplificar();
     return *this;
@@ -109,96 +113,126 @@ racional racional::operator/(const racional &r) const throw(error)
    que el racional r.*/
 bool racional::operator==(const racional &r) const throw()
 {
-    if(r._d == _d && r._n == _n){
+    if (r._d == _d && r._n == _n)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 bool racional::operator!=(const racional &r) const throw()
 {
-    if(r._d != _d && r._n != _n){
+    if (r._d != _d && r._n != _n)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 bool racional::operator<(const racional &r) const throw()
 {
     int p_entera = part_entera();
-    if(r.part_entera() < p_entera){
+    if (r.part_entera() < p_entera)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 bool racional::operator<=(const racional &r) const throw()
 {
     int p_entera = part_entera();
-    if(r.part_entera() <= p_entera){
+    if (r.part_entera() <= p_entera)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 bool racional::operator>(const racional &r) const throw()
 {
     int p_entera = part_entera();
-    if(r.part_entera() >= p_entera){
+    if (r.part_entera() >= p_entera)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 bool racional::operator>=(const racional &r) const throw()
 {
     int p_entera = part_entera();
-    if(r.part_entera() >= p_entera){
+    if (r.part_entera() >= p_entera)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
 int racional::mcd(int n1, int n1)
 {
-    int mcd(int n1, int n2) {
-  int hcf;
-  if (n2 > n1) {
-    int temp = n2;
-    n2 = n1;
-    n1 = temp;
-  }
+    int mcd(int n1, int n2)
+    {
+        int hcf;
+        if (n2 > n1)
+        {
+            int temp = n2;
+            n2 = n1;
+            n1 = temp;
+        }
 
-  for (int i = 1; i <= n2; ++i) {
-    if (n1 % i == 0 && n2 % i == 0) {
-      hcf = i;
+        for (int i = 1; i <= n2; ++i)
+        {
+            if (n1 % i == 0 && n2 % i == 0)
+            {
+                hcf = i;
+            }
+        }
+        if (hcf == 0)
+        {
+            hcf = n1 * n2;
+        }
+        return hcf;
     }
-  }
-  if (hcf == 0) {
-    hcf = n1 * n2;
-  }
-  return hcf;
-}
 }
 
-void racional::simplificar(){
+void racional::simplificar()
+{
     bool negatiu = false;
-    if (_d < 0) {
-    _d = _d * -1;
-    negatiu = true;
-  } else if(_n < 0 ){
-    _n = _n * -1;
-    negatiu = true;
-  }
-  int gcd = mcd(_n, _d);
-  if (gcd != 0) {
-    _n = _n / gcd;
-    _d = _d / gcd;
-  }
-    if(negatiu) _n = _n * -1;
+    if (_d < 0)
+    {
+        _d = _d * -1;
+        negatiu = true;
+    }
+    else if (_n < 0)
+    {
+        _n = _n * -1;
+        negatiu = true;
+    }
+    int gcd = mcd(_n, _d);
+    if (gcd != 0)
+    {
+        _n = _n / gcd;
+        _d = _d / gcd;
+    }
+    if (negatiu)
+        _n = _n * -1;
 }
 
-int racional::mcm(int a, int b){
-    return (a*b)/mcd(a, b);
+int racional::mcm(int a, int b)
+{
+    return (a * b) / mcd(a, b);
 }
