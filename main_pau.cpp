5d8083419579ac1ee9db7095a2f27c91c682cdb1
donpauu <pau.alcazar11@gmail.com>
2022-11-23 20:40:00 +0100

    pau

---
+++ main_pau.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+#include <utility>
+#include <iomanip>
+#include <cstdio>
+#include <esin/error>
+#include <esin/util>
+
+#include "racional.cpp"
+
+using namespace std;
+
+int main()
+{
+    
+}
