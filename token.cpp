0f59ce1658cd9298231446681d13c0a6d28546bb
donpauu <pau.alcazar11@gmail.com>
2022-12-12 17:23:15 +0100

    pau

--- token.cpp
+++ token.cpp
@@ -22,82 +22,82 @@ using namespace std;
 
 token::token(codi cod /*= NULLTOK*/) throw(error)
 {
-    _id = cod;
+  _id = cod;
 }
 token::token(int n) throw(error)
 {
-    _id = CT_ENTERA;
-    _valor._enter = n;
+  _id = CT_ENTERA;
+  _valor._enter = n;
 }
 token::token(const racional &r) throw(error)
 {
-    _id = CT_RACIONAL;
-    _valor._racional = r;
+  _id = CT_RACIONAL;
+  _valor._racional = r;
 }
 token::token(double x) throw(error)
 {
-    _id = CT_REAL;
-    _valor._real = x;
+  _id = CT_REAL;
+  _valor._real = x;
 }
 token::token(const string &var_name) throw(error)
 {
-    if (check_variables(var_name))
-    {
-        throw(11);
-    }
-    else
-    {
-        _id = VARIABLE;
-        _valor._variable = var_name;
-    }
+  if (check_variables(var_name))
+  {
+    throw(11);
+  }
+  else
+  {
+    _id = VARIABLE;
+    _valor._variable = var_name;
+  }
 }
 
 // Constructora por còpia, assignació i destructora.
 token::token(const token &t) throw(error)
 {
-    _id = t._id;
-    if (_id == CT_ENTERA)
-    {
-        _valor._enter = t._valor._enter;
-    }
-    else if (_id == CT_RACIONAL)
-    {
-        _valor._racional = t._valor._racional;
-    }
-    else if (_id == CT_REAL)
-    {
-        _valor._real = t._valor._real;
-    }
-    else if (_id == VARIABLE)
-    {
-        _valor._variable = t._valor._variable;
-    }
+  _id = t._id;
+  if (_id == CT_ENTERA)
+  {
+    _valor._enter = t._valor._enter;
+  }
+  else if (_id == CT_RACIONAL)
+  {
+    _valor._racional = t._valor._racional;
+  }
+  else if (_id == CT_REAL)
+  {
+    _valor._real = t._valor._real;
+  }
+  else if (_id == VARIABLE)
+  {
+    _valor._variable = t._valor._variable;
+  }
 }
 token &token::operator=(const token &t) throw(error)
 {
-    _id = t._id;
-    if (_id == CT_ENTERA)
-    {
-        _valor._enter = t._valor._enter;
-    }
-    else if (_id == CT_RACIONAL)
-    {
-        _valor._racional = t._valor._racional;
-    }
-    else if (_id == CT_REAL)
-    {
-        _valor._real = t._valor._real;
-    }
-    else if (_id == VARIABLE)
-    {
-        _valor._variable = t._valor._variable;
-    } // s'ha d'eliminar el _valor que te, en el cas que en tingui
+  _id = t._id;
+  if (_id == CT_ENTERA)
+  {
+    _valor._enter = t._valor._enter;
+  }
+  else if (_id == CT_RACIONAL)
+  {
+    _valor._racional = t._valor._racional;
+  }
+  else if (_id == CT_REAL)
+  {
+    _valor._real = t._valor._real;
+  }
+  else if (_id == VARIABLE)
+  {
+    _valor._variable = t._valor._variable;
+  } // s'ha d'eliminar el _valor que te, en el cas que en tingui
 
-    return *this;
+  return *this;
 }
 token::~token() throw()
 {
-    _id = NULLTOK;
+  _id = NULLTOK;
 }
 
 /*Consultores: Retornen respectivament el codi i el _valor (en el cas de
@@ -107,23 +107,23 @@ token::~token() throw()
   una CT_ENTERA.*/
 token::codi token::tipus() const throw()
 {
-    return _id;
+  return _id;
 }
 int token::valor_enter() const throw(error)
 {
-    return _valor._enter;
+  return _valor._enter;
 }
 racional token::valor_racional() const throw(error)
 {
-    return _valor._racional;
+  return _valor._racional;
 }
 double token::valor_real() const throw(error)
 {
-    return _valor._real;
+  return _valor._real;
 }
 string token::identificador_variable() const throw(error)
 {
-    return _valor._variable;
+  return _valor._variable;
 }
 
 /*Igualtat i desigualtat entre tokens. Dos tokens es consideren iguals si els
@@ -132,43 +132,43 @@ string token::identificador_variable() const throw(error)
   nom. */
 bool token::operator==(const token &t) const throw()
 {
-    bool ret = false;
-    if (_id != t._id)
+  bool ret = false;
+  if (_id != t._id)
+  {
+    throw(13);
+  }
+  if (_id == CT_ENTERA)
+  {
+    if (_id == t._id and _valor._enter == t._valor._enter)
     {
-        throw(13);
+      ret = true;
     }
-    if (_id == CT_ENTERA)
+    else
     {
-        if (_id == t._id and _valor._enter == t._valor._enter)
-        {
-            ret = true;
-        }
-        else
-        {
-            ret = false;
-        }
+      ret = false;
     }
-    return ret;
+  }
+  return ret;
 }
 bool token::operator!=(const token &t) const throw()
 {
-    bool ret = false;
-    if (_id != t._id)
+  bool ret = false;
+  if (_id != t._id)
+  {
+    throw(13);
+  }
+  if (_id == CT_ENTERA)
+  {
+    if (_id == t._id and _valor._enter != t._valor._enter)
     {
-        throw(13);
+      ret = true;
     }
-    if (_id == CT_ENTERA)
+    else
     {
-        if (_id == t._id and _valor._enter != t._valor._enter)
-        {
-            ret = true;
-        }
-        else
-        {
-            ret = false;
-        }
+      ret = false;
     }
-    return ret;
+  }
+  return ret;
 }
 
 /*Precedència entre tokens. L'operador > retorna cert si i només si el token
@@ -176,122 +176,122 @@ bool token::operator!=(const token &t) const throw()
   dels tokens no és un operador es produeix un error.*/
 bool token::operator>(const token &t) const throw(error)
 {
-    if((_id and t._id) != (SUMA or
-                           RESTA or
-                           MULTIPLICACIO or
-                           DIVISIO or
-                           CANVI_DE_SIGNE or
-                           SIGNE_POSITIU or
-                           EXPONENCIACIO or
-                           SQRT or
-                           LOG or
-                           EXP))
-        throw(14);
-    else
-    {
-        if (_id == (SUMA or RESTA))
-            return false;
-        else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == MULTIPLICACIO or
-                                                        DIVISIO or
-                                                        CANVI_DE_SIGNE or
-                                                        SIGNE_POSITIU or
-                                                        EXPONENCIACIO or
-                                                        SQRT or
-                                                        LOG or
-                                                        EXP))
-            return false;
-        else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == SUMA or
-                                                        RESTA))
-            return true;
-        else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == CANVI_DE_SIGNE or
-                                                            SIGNE_POSITIU or
-                                                            EXPONENCIACIO or
-                                                            SQRT or
-                                                            LOG or
-                                                            EXP))
-            return false;
-        else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == SUMA or
-                                                            RESTA or
-                                                            MULTIPLICACIO or
-                                                            DIVISIO))
-            return true;
-        else if (_id == (EXPONENCIACIO or SQRT or LOG or EXP) and (t._id == EXPONENCIACIO or
-                                                                SQRT or
-                                                                LOG or
-                                                                EXP))
-            return false;
-        else (_id == (EXPONENCIACIO or SQRT or LOG or EXP))
-            return true;
-    }
+  if ((_id and t._id) != (SUMA or
+                          RESTA or
+                          MULTIPLICACIO or
+                          DIVISIO or
+                          CANVI_DE_SIGNE or
+                          SIGNE_POSITIU or
+                          EXPONENCIACIO or
+                          SQRT or
+                          LOG or
+                          EXP))
+    throw(14);
+  else
+  {
+    if (_id == (SUMA or RESTA))
+      return false;
+    else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == MULTIPLICACIO or
+                                                    DIVISIO or
+                                                    CANVI_DE_SIGNE or
+                                                    SIGNE_POSITIU or
+                                                    EXPONENCIACIO or
+                                                    SQRT or
+                                                    LOG or
+                                                    EXP))
+      return false;
+    else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == SUMA or
+                                                    RESTA))
+      return true;
+    else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == CANVI_DE_SIGNE or
+                                                           SIGNE_POSITIU or
+                                                           EXPONENCIACIO or
+                                                           SQRT or
+                                                           LOG or
+                                                           EXP))
+      return false;
+    else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == SUMA or
+                                                           RESTA or
+                                                           MULTIPLICACIO or
+                                                           DIVISIO))
+      return true;
+    else if (_id == (EXPONENCIACIO or SQRT or LOG or EXP) and (t._id == EXPONENCIACIO or
+                                                               SQRT or
+                                                               LOG or
+                                                               EXP))
+      return false;
+    else if
+      (_id == (EXPONENCIACIO or SQRT or LOG or EXP)) return true;
+  }
 }
 bool token::operator<(const token &t) const throw(error)
 {
-    if((_id and t._id) != (SUMA or
-                           RESTA or
-                           MULTIPLICACIO or
-                           DIVISIO or
-                           CANVI_DE_SIGNE or
-                           SIGNE_POSITIU or
-                           EXPONENCIACIO or
-                           SQRT or
-                           LOG or
-                           EXP))
-        throw(14);
-    else
-    {
-        if (_id == (SUMA or RESTA) and (t._id == MULTIPLICACIO or
-                                        DIVISIO or
-                                        CANVI_DE_SIGNE or
-                                        SIGNE_POSITIU or
-                                        EXPONENCIACIO or
-                                        SQRT or
-                                        LOG or
-                                        EXP))
-            return true;
-        else if (_id == (SUMA or RESTA) and (t._id == SUMA or
-                                            RESTA))
-            return false;
-        else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == CANVI_DE_SIGNE or
-                                                        SIGNE_POSITIU or
-                                                        EXPONENCIACIO or
-                                                        SQRT or
-                                                        LOG or
-                                                        EXP))
-            return true;
-        else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == SUMA or
-                                                        RESTA or
-                                                        MULTIPLICACIO or
-                                                        DIVISIO))
-            return false;
-        else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == SQRT or
-                                                            LOG or
-                                                            EXP))
-            return true;
-        else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == SUMA or
-                                                            RESTA or
-                                                            MULTIPLICACIO or
-                                                            DIVISIO or
-                                                            CANVI_DE_SIGNE or
-                                                            SIGNE_POSITIU))
-            return false;
-        else if (_id == (EXPONENCIACIO or SQRT or LOG or EXP))
-            return false;
-    }
+  if ((_id and t._id) != (SUMA or
+                          RESTA or
+                          MULTIPLICACIO or
+                          DIVISIO or
+                          CANVI_DE_SIGNE or
+                          SIGNE_POSITIU or
+                          EXPONENCIACIO or
+                          SQRT or
+                          LOG or
+                          EXP))
+    throw(14);
+  else
+  {
+    if (_id == (SUMA or RESTA) and (t._id == MULTIPLICACIO or
+                                    DIVISIO or
+                                    CANVI_DE_SIGNE or
+                                    SIGNE_POSITIU or
+                                    EXPONENCIACIO or
+                                    SQRT or
+                                    LOG or
+                                    EXP))
+      return true;
+    else if (_id == (SUMA or RESTA) and (t._id == SUMA or
+                                         RESTA))
+      return false;
+    else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == CANVI_DE_SIGNE or
+                                                    SIGNE_POSITIU or
+                                                    EXPONENCIACIO or
+                                                    SQRT or
+                                                    LOG or
+                                                    EXP))
+      return true;
+    else if (_id == (MULTIPLICACIO or DIVISIO) and (t._id == SUMA or
+                                                    RESTA or
+                                                    MULTIPLICACIO or
+                                                    DIVISIO))
+      return false;
+    else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == SQRT or
+                                                           LOG or
+                                                           EXP))
+      return true;
+    else if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU) and (t._id == SUMA or
+                                                           RESTA or
+                                                           MULTIPLICACIO or
+                                                           DIVISIO or
+                                                           CANVI_DE_SIGNE or
+                                                           SIGNE_POSITIU))
+      return false;
+    else if (_id == (EXPONENCIACIO or SQRT or LOG or EXP))
+      return false;
+  }
 }
 
 bool token::check_variables(string var)
 {
-    char lletra;
-    bool incorrecte = false;
-    for (unsigned int i = 0; i < var.length(); i++)
+  char lletra;
+  bool incorrecte = false;
+  for (unsigned int i = 0; i < var.length(); i++)
+  {
+    lletra = var[i];
+    if (!isalpha(lletra) and int(lletra) != 95)
     {
-        lletra = var[i];
-        if (!isalpha(lletra) and int(lletra) != 95)
-        {
-            incorrecte = true;
-        }
+      incorrecte = true;
     }
-    return incorrecte;
+  }
+  return incorrecte;
 }
 /*
 int token::prioritat_operacio()
@@ -317,12 +317,12 @@ int token::numero_operadors()
 //       actua. Si el token no és cap operador retornarà un 0, per exemple en el cas
 //       de les constants o variables. Si el token no és vàlid en una expressió retorna -1.
 {
-    if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU or SQRT or LOG or EXP))
-        return 1;
-    else if (_id == (SUMA or RESTA or MULTIPLICACIO or DIVISIO or EXPONENCIACIO))
-        return 2;
-    else if (_id == (CT_ENTERA or CT_RACIONAL or CT_REAL or CT_E or VARIABLE))
-        return 0;
-    else
-        return -1;
+  if (_id == (CANVI_DE_SIGNE or SIGNE_POSITIU or SQRT or LOG or EXP))
+    return 1;
+  else if (_id == (SUMA or RESTA or MULTIPLICACIO or DIVISIO or EXPONENCIACIO))
+    return 2;
+  else if (_id == (CT_ENTERA or CT_RACIONAL or CT_REAL or CT_E or VARIABLE))
+    return 0;
+  else
+    return -1;
 }
