8d8f1e518acef4498a3eb0e1a71a78b59b851abd
Cesconcins <cesco.oncins@gmail.com>
2023-01-03 10:20:30 +0100

    Cesco

--- token.cpp
+++ token.cpp
@@ -21,7 +21,7 @@ using namespace std;
   coincidir amb un nom reservat: unassign, e, sqrt, log, exp i evalf.*/
 
 
-token::data::data()
+/*token::data::data()
 {
 }
 
@@ -35,7 +35,7 @@ data & token::data::operator=(const data & d) throw();
 
 token::data::~data() throw();
 {
-}
+}*/
 
 token::token(codi cod /*= NULLTOK*/) throw(error)
 {
@@ -47,21 +47,24 @@ token::token(codi cod /*= NULLTOK*/) throw(error)
 token::token(int n) throw(error)
 {
   _id = CT_ENTERA;
-  _valor._enter = n;
-  //_valor = &n;
+  //_valor._enter = n;
+  _valor = new int;
+  _valor = &n;
 }
 token::token(const racional &r) throw(error)
 {
   _id = CT_RACIONAL;
-  _valor._racional = r;
-  //racional rac = r;
-  //_valor = &rac;
+  //_valor._racional = r;
+  _valor = new racional;
+  racional rac = r;
+  _valor = &rac;
 }
 token::token(double x) throw(error)
 {
   _id = CT_REAL;
-  _valor._real = x;
-  //_valor = &x;
+  //_valor._real = x;
+  _valor = new double;
+  _valor = &x;
 }
 token::token(const string &var_name) throw(error)
 {
@@ -73,8 +76,9 @@ token::token(const string &var_name) throw(error)
   else
   {
     _id = VARIABLE;
-    _valor._variable = var_name;
-    //_valor = &s;
+    //_valor._variable = var_name;
+    _valor = new string;
+    _valor = &s;
   }
 }
 
@@ -147,23 +151,23 @@ token::codi token::tipus() const throw()
 }
 int token::valor_enter() const throw(error)
 {
-  return _valor._enter;
-  //return *(int *)_valor;
+  //return _valor._enter;
+  return *(int *)_valor;
 }
 racional token::valor_racional() const throw(error)
 {
-  return _valor._racional;
-  //return *(racional *)_valor;
+  //return _valor._racional;
+  return *(racional *)_valor;
 }
 double token::valor_real() const throw(error)
 {
-  return _valor._real;
-  //return *(double *)_valor;
+  //return _valor._real;
+  return *(double *)_valor;
 }
 string token::identificador_variable() const throw(error)
 {
-  return _valor._variable;
-  //return *(string *)_valor;
+  //return _valor._variable;
+  return *(string *)_valor;
 }
 
 /*Igualtat i desigualtat entre tokens. Dos tokens es consideren iguals si els
@@ -210,23 +214,24 @@ bool token::operator>(const token &t) const throw(error)
     if (_id == SUMA or _id == RESTA)
       return false;
     else if ((_id == MULTIPLICACIO or _id == DIVISIO) and
-             (t._id == MULTIPLICACIO or t._id == DIVISIO or t._id == CANVI_DE_SIGNE or
-              t._id == SIGNE_POSITIU or t._id == EXPONENCIACIO or t._id == SQRT or t._id == LOG or t._id == EXP))
+            (t._id != SUMA and t._id != RESTA))
       return false;
     else if ((_id == MULTIPLICACIO or _id == DIVISIO) and (t._id == SUMA or t._id == RESTA))
       return true;
     else if ((_id == CANVI_DE_SIGNE or _id == SIGNE_POSITIU) and
-             (t._id == CANVI_DE_SIGNE or t._id == SIGNE_POSITIU or t._id == EXPONENCIACIO or
-              t._id == SQRT or t._id == LOG or t._id == EXP))
+            (t._id != SUMA and t._id != RESTA and t._id != MULTIPLICACIO and t._id != DIVISIO))
       return false;
     else if ((_id == CANVI_DE_SIGNE or _id == SIGNE_POSITIU) and
-    (t._id == SUMA or t._id == RESTA or t._id == MULTIPLICACIO or t._id == DIVISIO))
+            (t._id == SUMA or t._id == RESTA or t._id == MULTIPLICACIO or t._id == DIVISIO))
       return true;
     else if ((_id == EXPONENCIACIO or _id == SQRT or _id == LOG or _id == EXP) and
-    (t._id == EXPONENCIACIO or t._id == SQRT or t._id == LOG or t._id == EXP))
+            (t._id == EXPONENCIACIO or t._id == SQRT or t._id == LOG or t._id == EXP 
+            or t._id == OBRIR_PAR or t._id == TANCAR_PAR))
       return false;
-    else if (_id == EXPONENCIACIO or _id == SQRT or _id == LOG or _id == EXP)
+    else if ((_id == EXPONENCIACIO or _id == SQRT or _id == LOG or _id == EXP) and
+            (t._id != OBRIR_PAR and t._id != TANCAR_PAR))
       return true;
+    else return false;
   }
 }
 bool token::operator<(const token &t) const throw(error)
@@ -241,26 +246,27 @@ bool token::operator<(const token &t) const throw(error)
   else
   {
     if ((_id == SUMA or _id == RESTA) and
-        (t._id == MULTIPLICACIO or t._id == DIVISIO or t._id == CANVI_DE_SIGNE or
-        t._id == SIGNE_POSITIU or t._id == EXPONENCIACIO or t._id == SQRT or t._id == LOG or t._id == EXP))
+       (t._id != SUMA and t._id != RESTA))
       return true;
     else if ((_id == SUMA or _id == RESTA) and (t._id == SUMA or t._id == RESTA))
       return false;
     else if ((_id == MULTIPLICACIO or _id == DIVISIO) and
-             (t._id == CANVI_DE_SIGNE or t._id == SIGNE_POSITIU or t._id == EXPONENCIACIO or
-              t._id == SQRT or t._id == LOG or t._id == EXP))
+            (t._id != SUMA and t._id != RESTA and t._id != MULTIPLICACIO and t._id != DIVISIO))
       return true;
     else if ((_id == MULTIPLICACIO or _id == DIVISIO) and
-             (t._id == SUMA or t._id == RESTA or t._id == MULTIPLICACIO or t._id == DIVISIO))
+            (t._id == SUMA or t._id == RESTA or t._id == MULTIPLICACIO or t._id == DIVISIO))
       return false;
     else if ((_id == CANVI_DE_SIGNE or _id == SIGNE_POSITIU) and
-    (t._id == SQRT or t._id == LOG or t._id == EXP))
+            (t._id == SQRT or t._id == LOG or t._id == EXP or t._id == OBRIR_PAR or t._id == TANCAR_PAR))
       return true;
     else if ((_id == CANVI_DE_SIGNE or _id == SIGNE_POSITIU) and
-    (t._id == SUMA or RESTA or MULTIPLICACIO or DIVISIO or CANVI_DE_SIGNE or SIGNE_POSITIU))
+            (t._id == SUMA or t._id == RESTA or t._id == MULTIPLICACIO or t._id == DIVISIO or
+            t._id == CANVI_DE_SIGNE or t._id == SIGNE_POSITIU))
       return false;
-    else if (_id == EXPONENCIACIO or _id == SQRT or _id == LOG or _id == EXP)
-      return false;
+    else if ((_id == EXPONENCIACIO or _id == SQRT or _id == LOG or _id == EXP) and
+            (t._id == OBRIR_PAR or t._id == TANCAR_PAR))
+      return true;
+    else return false;
   }
 }
 
