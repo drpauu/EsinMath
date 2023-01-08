bfd5630a63a068e45a06e86581536269ae5def58
donpauu <pau.alcazar11@gmail.com>
2023-01-08 14:47:43 +0100

    pau

--- variables.cpp
+++ variables.cpp
@@ -2,28 +2,83 @@
 
 using std::map;
 
-// Construeix un conjunt de variables buit.
+// Constructor
 variables::variables() throw(error)
 {
-    node v;
-    node
+    node *v;
+    v = new node;
+    v->seg = nullptr;
+    primer = v;
 }
 
-// Constructora per còpia, assignació i destructora.
+// Copy constructor
 variables::variables(const variables &v) throw(error)
 {
-    _var = v._var;
+    // Create a new node to serve as the head of the new list
+    node *new_head = new node;
+    new_head->seg = nullptr;
+    primer = new_head;
+
+    // Create a pointer to traverse the list
+    node *current = v.primer;
+
+    // Copy each node in the list
+    while (current != nullptr)
+    {
+        primer->seg = new node;
+        primer->seg->info = current->info;
+        primer = primer->seg;
+        current = current->seg;
+    }
 }
 
+// Assignment operator
 variables &variables::operator=(const variables &v) throw(error)
 {
-    _var = v._var;
+    // Check for self-assignment
+    if (this == &v)
+    {
+        return *this;
+    }
+
+    // Clear the current list
+    node *current = primer;
+    while (current != nullptr)
+    {
+        node *temp = current;
+        current = current->seg;
+        delete temp;
+    }
+
+    // Create a new head node
+    node *new_head = new node;
+    new_head->seg = nullptr;
+    primer = new_head;
+
+    // Copy each node in the list
+    current = v.primer;
+    while (current != nullptr)
+    {
+        primer->seg = new node;
+        primer->seg->info = current->info;
+        primer = primer->seg;
+        current = current->seg;
+    }
+
     return *this;
 }
+
+// Destructor
 variables::~variables() throw(error)
 {
-    map<string, expressio> buit;
-    _var = buit;
+    // Delete each node in the list
+    node *current = primer;
+    while (current != nullptr)
+    {
+        node *temp = current;
+        current = current->seg;
+        delete temp;
+    }
 }
 
 /* Afegeix al conjunt de variables la variable de nom v juntament amb el seu
@@ -31,27 +86,50 @@ variables::~variables() throw(error)
    valor-expressió. */
 void variables::assign(const string &v, const expressio &e) throw(error)
 {
-    map<string, expressio>::iterator it;
-    it = _var.find(v);
-    if (it != _var.end())
+    // Check if the variable already exists
+    node* current = primer;
+    while (current != nullptr)
     {
-        it->second = e;
+        if (current->info.first == v)
+        {
+            // If it exists, assign the new value
+            current->info.second = e;
+            return;
+        }
+        current = current->seg;
     }
-    else
+
+    // If the variable does not exist, create a new node and add it to the list
+    node* new_node = new node;
+    new_node->seg = nullptr;
+    new_node->info = std::make_pair(v, e);
+
+    // Add the new node to the end of the list
+    current = primer;
+    while (current->seg != nullptr)
     {
-        _var.insert(it, std::pair<string, expressio>(v, e));
+        current = current->seg;
     }
+    current->seg = new_node;
 }
 
 /* Elimina del conjunt de variables el parell amb la variable de nom v. Si
    la variable no existeix llavors no fa res. */
 void variables::unassign(const string &v) throw()
 {
-    map<string, expressio>::iterator it;
-    it = _var.find(v);
-    if (it != _var.end())
+    // Check if the variable exists
+    node* current = primer;
+    while (current != nullptr)
     {
-        _var.erase(it);
+        if (current->info.first == v)
+        {
+            // If it exists, delete the node
+            node* temp = current;
+            current = current->seg;
+            delete temp;
+            return;
+        }
+        current = current->seg;
     }
 }
 
@@ -59,27 +137,31 @@ void variables::unassign(const string &v) throw()
    el conjunt de variables retorna l'expressió buida. */
 expressio variables::valor(const string &lv) const throw(error)
 {
-    if (_var.find(lv) != _var.end())
+    // Search for the variable
+    node* current = primer;
+    while (current != nullptr)
     {
-        return _var.find(lv)->second;
+        if (current->info.first == lv)
+        {
+            // If the variable is found, return its value
+            return current->info.second;
+        }
+        current = current->seg;
     }
-    else
-    {
-        expressio a;
-        return a;
-    }
+
+    // If the variable is not found, return an empty expression
+    expressio e;
+    return e;
 }
 
 /* Retorna en l totes les claus del conjunt de variables, en un ordre
    qualsevol. Si no hi ha cap clau retorna la llista buida.*/
 void variables::dump(list<string> &l) const throw(error)
 {
-    map<string, expressio> copia = _var;
-    for (map<string, expressio>::iterator it = copia.begin(); it != copia.end(); it++)
+    node* current = primer;
+    while (current != nullptr)
     {
-        l.push_back(it->first);
+        l.push_back(current->info.first);
+        current = current->seg;
     }
-    // return l; en el post diu que s ha de retornar una llista,
-    // pero es una fucnio void, no ho entenc.
-    // ho poso com a comentari
 }
